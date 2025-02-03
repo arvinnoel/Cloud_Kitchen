7a8b03f6c1307f8b01abc1f1aaa1755d37c14291
Arvin <noelarvin96@gmail.com>
2025-02-03 11:37:01 +0530

    fixed toast configure

--- Frontend/src/customerpage/Cart.jsx
+++ Frontend/src/customerpage/Cart.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-toast.configure();
 
 const Cart = () => {
   const [cart, setCart] = useState([]);
